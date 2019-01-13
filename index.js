48d2d4ee754b2eb769161825faab758debe13d77
Jaimin Desai <judesai@gmail.com>
2019-01-13 14:09:39 +0530

    Added giphy.gif

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const API_URL = "https://starwars.egghead.training/";
 const output = document.getElementById("output");
-
+const spinner = document.getElementById("spinner");
 getFilmsTitle = films => {
   return films
     .slice()
     .sort((a, b) => a.episode_id - b.episode_id)
-    .map(film => `${film.episode_id} . ${film.title}`)
+    .map(film => (film ? `${film.episode_id} . ${film.title}` : null))
     .join("\n");
 };
 
 fetch(API_URL + "films")
-  .then(response => response.json())
-  .then(films => {
-    output.innerText = getFilmsTitle(films);
+  .then(response => {
+    if (!response.ok) {
+      return Promise.reject(new Error("Unsuccessful response"));
+    }
+    return response.json().then(films => {
+      output.innerText = getFilmsTitle(films);
+    });
+  })
+  .catch(error => {
+    console.warn(error);
+    output.innerText = error;
+  })
+  .finally(() => {
+    spinner.remove();
   });
